0e74ae62b12bc73e13bd39a83bce5d879e141ca8
Krzysztof Bojakowski <krzysztof0bojakowski@gmail.com>
2015-03-24 12:33:26 +0100

    Grid done, Adding done
    //ToDo
    -Edit
    -Search
    -Delete
    -Repair layout

---
+++ KitchenDbApp/AddWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Objects;
+using System.Data;
+
+namespace KitchenDbApp
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Potrawy recepie = new Potrawy();
+            recepie.NazwaPotrawy = NameTextBox.Text;
+            recepie.Skladniki = IngTextBox.Text;
+
+            using (var dbCtx = new KitchenEntities())
+            {
+                dbCtx.Entry(recepie).State = System.Data.EntityState.Added;
+                dbCtx.SaveChanges();
+            }
+            this.Close();
+        }
+    }
+}
---
+++ KitchenDbApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Objects;
+
+namespace KitchenDbApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KitchenEntities dataEntities = new KitchenEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //ObjectQuery<Potrawy> recepies = dataEntities.Potrawy;
+
+            var query =
+            from recepie in dataEntities.Potrawies
+            select new { recepie.IdPotrawy, recepie.NazwaPotrawy, recepie.Skladniki };
+            
+            DataGrid.ItemsSource = query.ToList();
+        }
+
+        private void AddRecepie_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new AddWindow();
+            //newWindow.Show();
+            newWindow.ShowDialog();
+            Window_Loaded(sender, e);
+        }
+    }
+}
